ad3ed0b78097d7bb5fae8acd93437af5168b7198
Aditria Pardana <aditria.pardana20@gmail.com>
2022-09-22 22:05:38 +0700

    update component Section

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 import Navigation from "./components/Navigation";
 import Banner from "./components/Banner";
+import Skills from "./components/Skills";
 
 function App() {
   return (
     <div className="App">
       <Navigation />
       <Banner />
+      <Skills />
     </div>
   );
 }
