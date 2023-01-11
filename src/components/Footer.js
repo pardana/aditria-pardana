79c90647c96d293d7f6f114ba0c8af775af83268
Aditria Pardana <aditria.pardana20@gmail.com>
2023-01-11 11:53:53 +0700

    update link icon

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,9 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import logo from "../assets/img/logo.svg";
-import navIcon1 from "../assets/img/nav-icon1.svg";
-import navIcon2 from "../assets/img/nav-icon2.svg";
-import navIcon3 from "../assets/img/nav-icon3.svg";
 import moment from "moment";
+import { BsWhatsapp, BsInstagram, BsLinkedin } from "react-icons/bs";
+import { DiGithubBadge } from "react-icons/di";
 
 const dateTime = new Date();
 
@@ -19,14 +18,26 @@ export default function Footer() {
           </Col>
           <Col sm={6} className="text-center text-sm-end">
             <div className="social-icon">
-              <a href="">
-                <img src={navIcon1} />
+              <a
+                style={{ color: "white" }}
+                href="https://www.linkedin.com/in/aditria-pardana-b846a9121/"
+                target="_blank"
+              >
+                <BsLinkedin />
               </a>
-              <a href="">
-                <img src={navIcon2} />
+              <a
+                style={{ color: "white" }}
+                href="https://www.instagram.com/appardana/"
+                target="_blank"
+              >
+                <BsInstagram />
               </a>
-              <a href="">
-                <img src={navIcon3} />
+              <a
+                style={{ color: "white", fontSize: 18 }}
+                href="https://github.com/pardana"
+                target="_blank"
+              >
+                <DiGithubBadge />
               </a>
             </div>
             <p>
