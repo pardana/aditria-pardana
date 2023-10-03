361bdff421a4724f4552d2f59a7f9838cb7d30fe
Aditria Pardana <aditria.pardana20@gmail.com>
2023-10-03 16:30:20 +0700

    add ig GA

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import ReactGA from "react-ga4";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+ReactGA.initialize("G-HY2SYQ2Y4L");
+
+// Send pageview with a custom path
+ReactGA.send({
+  hitType: "pageview",
+  page: window.location.pathname,
+  title: window.location.pathname,
+});
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
